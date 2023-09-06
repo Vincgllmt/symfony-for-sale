f5641b5150d68b495326d2b17ef05633e80dde18
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2023-09-06 16:03:27 +0200

    feat:  added fixtures and factory for advertisement

---
+++ src/DataFixtures/AdvertisementFixtures.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\AdvertisementFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AdvertisementFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        AdvertisementFactory::createMany(500);
+    }
+}
