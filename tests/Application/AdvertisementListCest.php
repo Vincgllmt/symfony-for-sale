2d3413e45f1c83776e984082717a300d7c792b02
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-10-11 16:13:01 +0200

    feat: test listOf15: OK

--- tests/Application/AdvertisementListCest.php
+++ tests/Application/AdvertisementListCest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Application;
 
+use App\Factory\AdvertisementFactory;
+use App\Factory\CategoryFactory;
 use App\Tests\Support\ApplicationTester;
 
 class AdvertisementListCest
@@ -10,5 +12,16 @@ class AdvertisementListCest
     {
         $I->amOnPage('/advertisement');
         $I->seeResponseCodeIsSuccessful();
+    }
+
+    public function listOf15(ApplicationTester $I)
+    {
+        CategoryFactory::createOne();
+        AdvertisementFactory::createMany(15);
+        $I->amOnPage('/advertisement');
+        $I->seeElement('.current');
+        $I->seeElement('.page');
+        $I->seeElement('.last');
+        $I->seeNumberOfElements('main .list-group .list-group-item', 10/* only 10, pagination ftw */);
     }
 }
