6db6f5ad5ea78906bbd6a39e05011533f748d30a
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-10-18 13:57:54 +0200

    test: pageIsAvailable (OK)

---
+++ tests/Application/AvailabilityCest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tests\Application;
+
+use App\Factory\AdvertisementFactory;
+use App\Factory\CategoryFactory;
+use App\Tests\Support\ApplicationTester;
+use Codeception\Attribute\DataProvider;
+use Codeception\Example;
+
+class AvailabilityCest
+{
+    public function _before(ApplicationTester $I)
+    {
+        $catg = CategoryFactory::createOne();
+        AdvertisementFactory::createOne(['category' => $catg]);
+    }
+
+    #[DataProvider('pageIsAvailableProvider')]
+    public function pageIsAvailable(ApplicationTester $I, Example $example)
+    {
+        $I->amOnPage($example[0]);
+        $I->seeResponseCodeIsSuccessful();
+    }
+
+    public function pageIsAvailableProvider()
+    {
+        return [
+            ['/advertisement'],
+            ['/advertisement/new'],
+            ['/advertisement/1'],
+            ['/advertisement/edit/1'],
+            ['/advertisement/delete/1'],
+            ['/category'],
+            ['/category/1'],
+        ];
+    }
+}
