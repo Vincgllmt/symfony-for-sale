a690d21cb8ccf39674141a19c741be35c4499e75
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-12-13 16:18:01 +0100

    fix: purgeRegistrationWithDay

--- tests/Application/Command/PurgeRegistrationCest.php
+++ tests/Application/Command/PurgeRegistrationCest.php
@@ -12,10 +12,31 @@ class PurgeRegistrationCest
         $users = UserFactory::createMany(10, ['isVerified' => false]);
         $repository = UserFactory::repository();
 
-        $I->runSymfonyConsoleCommand('app:purge-registration', ['--delete' => true, '--force' => true]);
+        $I->runSymfonyConsoleCommand('app:purge-registration', ['--delete' => true, '--force' => true], ['']);
 
         foreach ($users as $user) {
             $repository->assert()->notExists([
+                'id' => $user->getId(),
+            ]);
+        }
+    }
+
+    public function purgeRegistrationWithDay(ApplicationTester $I): void
+    {
+        $usersNotToDelete = UserFactory::createMany(10, ['isVerified' => false, 'registeredAt' => new \DateTime('-4 days')]);
+        $usersToDelete = UserFactory::createMany(10, ['isVerified' => false, 'registeredAt' => new \DateTime('-1 days')]);
+        $repository = UserFactory::repository();
+
+        $I->runSymfonyConsoleCommand('app:purge-registration', ['--delete' => true, '--force' => true, 'days' => 2], ['']);
+
+        foreach ($usersToDelete as $user) {
+            $repository->assert()->notExists([
+                'id' => $user->getId(),
+            ]);
+        }
+
+        foreach ($usersNotToDelete as $user) {
+            $repository->assert()->exists([
                 'id' => $user->getId(),
             ]);
         }
