694638211a800becaf0b02265fbed7cc4155a63c
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-12-13 17:26:57 +0100

    fix: bundle stimulus

--- src/Twig/Components/AdvertisementLikes.php
+++ src/Twig/Components/AdvertisementLikes.php
@@ -4,11 +4,14 @@ namespace App\Twig\Components;
 
 use App\Entity\Advertisement;
 use App\Entity\User;
-use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
 
-#[AsTwigComponent()]
+#[AsLiveComponent]
 final class AdvertisementLikes
 {
+    use DefaultActionTrait;
     public Advertisement $adv;
     public ?User $user;
 
@@ -24,5 +27,19 @@ final class AdvertisementLikes
         }
 
         return $this->adv->getLikes()->contains($this->user);
+    }
+
+    #[LiveAction]
+    public function toggleLike(): void
+    {
+        if (!$this->user) {
+            return;
+        }
+
+        if ($this->isLikedByUser()) {
+            $this->adv->removeLike($this->user);
+        } else {
+            $this->adv->addLike($this->user);
+        }
     }
 }
